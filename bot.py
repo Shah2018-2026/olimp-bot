7fdabbb3e442f6b70782b3d1c1b16a14c0b99823
Shah2018-2026 <yugai_pavel_73@mail.ru>
2026-06-21 16:15:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 import time
 import pyautogui
-import webbrowser
+import subprocess
 
 STAVKA = 30
 MIN_KOEF = 1.8
@@ -12,6 +12,8 @@ STAVKA_X = 1215
 STAVKA_Y = 369
 
 COOKIE = "secured=1; OASPRD4=e292230b-bcf2-475f-a299-7e89b890eba7; BWPRD4=801060d1-4635-49a3-92f7-b6d03e3d6b7e; cf_clearance=g4HUNUT4PaXBEWa2hcr1Tri1dhgFbpRw5StL0Sa9hvI-1782020658-1.2.1.1-YRNF.eJdXsg8wdeh4MNw0FTIBNY1CxnlkfpI5Sva1E.3pB5brk_fuVKgDpgvPzoTB_9aNb9ZEeH5BpGPwJHJdj3.bhX4kCkZE59SN5TSxiQSOqnXIgJeAJfo84.qQ6QO2BO30nUJlZTKMswkJkP0c96fC.v4hBJkZUd7_8X5KbYbni6aJHHXlbeZ4bU070CTWBCRA2YdZZ9U7jyOtsyd87DiDyfYJd9j0u55774MxXxLkwT1KT3LMEYRoNNlCHIFGZ4aQOqPBfSZYOk1xe54NjyMvDWTZqd3W65sM_E476DS36vvw7XES0ww_F.EwaIftAk9tLOhgPwkgQzMEcj5ZQ"
+
+CHROME = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 session = requests.Session()
 session.headers.update({
@@ -76,10 +78,10 @@ while True:
             name = m.get("name", "?")
             match_url = "https://old.olimpbet.kz/live/events/?ids=" + str(mid)
             print("NAJDEN:", name, score_str)
-            print("Otkryvayu:", match_url)
+            print("Otkryvayu v Chrome:", match_url)
             stavki.append(mid)
-            webbrowser.open(match_url)
-            time.sleep(8)
+            subprocess.Popen([CHROME, match_url])
+            time.sleep(10)
             pyautogui.click(SUMMA_X, SUMMA_Y)
             time.sleep(1)
             pyautogui.hotkey('ctrl', 'a')
